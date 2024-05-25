899ac34b40f04d8fcb21af6d7c480f58ca3a5b33
harsh29patel <harshpatel290703@gmail.com>
2024-05-25 12:15:28 +0530

    a new component created and understood

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,17 @@
-
+import Chai from "./chai"
 
 function App() {
  
 
   return (
-  <h1> chai aur react with vite | harsh patel</h1>
+    <>   
+     {/* you can write empty tag also if you don't want to write div    */}
+  <Chai/>
+  <h1> harsh </h1>
+  <p> patel </p>   {/* you can export one element only so create div if you want to return more than one element */}
+  </>
   )
+  
 }
 
 export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,9 @@
+// file name is always end with jsx 
+
+function Chai(){   // always write capital letter first {this file is actually caleed a component}
+    return(
+        <h3> chai is ready</h3>
+    )
+}
+
+export default Chai
