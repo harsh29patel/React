8a32dee5b60273c03b41f93be3fa50b0b25751bf
harsh29patel <harshpatel290703@gmail.com>
2024-05-26 11:55:56 +0530

    variable and element declaration

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,12 +2,13 @@ import Chai from "./chai"
 
 function App() {
  
+  const username = "harsh patel"
 
   return (
     <>   
      {/* you can write empty tag also if you don't want to write div    */}
   <Chai/>
-  <h1> harsh </h1>
+  <h1> harsh  {username} </h1> {/*you can add variable in {} brackets      this is also called evaluated expression */}
   <p> patel </p>   {/* you can export one element only so create div if you want to return more than one element */}
   </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+ 
+
+function MyApp(){
+   return ( <h1>harsh</h1>)
+}
 
 
+
+// const ReactElement =    
+// {
+//     type:'a',
+//     props:{
+//         href: 'https://google.com',      thid code will not run 
+//         target:'_blank'            
+//     },
+//     children:'Click on me to visit google'
+// } 
+
+const anotherElement = (
+    <a href='https://google.com' target='_blank'>click here to visit google</a>
+)
+
+const anotherUser = "chai aur code"
+
+const reactElement = React.createElement(
+    'a',{
+        href:'hhtps://google.com' , target:'_blank'
+    },
+    'click me to vist google',
+    anotherUser
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <App />
+    reactElement
 
 )
