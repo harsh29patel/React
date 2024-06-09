842a1fd6dbde14c4e9dd17d61418ad142a028296
harsh29patel <harshpatel290703@gmail.com>
2024-06-09 09:28:09 +0530

    megablog

--- 12MedgaBlock/src/appwrite/configuration.js
+++ 12MedgaBlock/src/appwrite/configuration.js
@@ -13,6 +13,61 @@ constructor(){
     this.databases = new Databases(this.client);
     this.bucket = new Storage(this.client)
 }
+
+
+    async createPost({title,slug,content,featuredImage , status , userId}){
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDataBaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+        } catch (error) {
+         console.log("Appwrite service :: createPost :: error",error);   
+        }
+    }
+
+    async updatePost(slug,{title,content,featuredImage , status }){
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDataBaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                }
+            );
+        } catch (error) {
+            console.log("Appwrite service :: updatePost :: error" , error);
+        }
+    }
+
+    
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDataBaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+
+            return true
+        } catch (error) {
+    console.log("Appwrite serive :: deletePost :: error",error);
+            return false
+     }
+    }
+
 }
 
 
